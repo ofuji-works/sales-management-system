b744d87a1975021d07b9da74281aa4d6e2048975
ofuji-works <ofuji.works@gmail.com>
2023-03-16 02:08:25 +0900

    feat: main

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,37 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+pub mod adapters;
+pub mod application;
+pub mod domain;
+pub mod infrastructure;
+
+use tauri::Manager;
+
+use crate::infrastructure::{database, tauri::product};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sqlite_pool = database::excute()?;
+
+    tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+                window.close_devtools();
+            }
+            Ok(())
+        })
+        .manage(sqlite_pool)
+        .invoke_handler(tauri::generate_handler![
+            product::create_product,
+            product::search_product
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
+    Ok(())
+}
