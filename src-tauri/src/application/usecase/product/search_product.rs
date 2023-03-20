b8002a5bd9211a98923e5726d8a58e12842aa9a4
ofuji-works <ofuji.works@gmail.com>
2023-03-20 23:16:20 +0900

    test: usecase at search_product test

--- src-tauri/src/application/usecase/product/search_product.rs
+++ src-tauri/src/application/usecase/product/search_product.rs
@@ -6,6 +6,7 @@ use crate::{
     domain::product::Product,
 };
 
+#[derive(Debug)]
 pub struct SearchProductInput {}
 impl SearchProductInput {
     pub fn new() -> Self {
@@ -13,7 +14,7 @@ impl SearchProductInput {
     }
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 pub struct SearchProductOutput {
     pub products: Vec<Product>,
 }
@@ -39,5 +40,48 @@ impl SearchProductUsecase {
         let products = self.repository.search().await?;
         let output = SearchProductOutput::new(products);
         Ok(output)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use sqlx::SqlitePool;
+
+    use crate::{
+        adapters::gateway::product_repository::SqliteProductRepository,
+        application::usecase::product::search_product::{SearchProductInput, SearchProductUsecase},
+        infrastructure::database::MIGRATOR,
+    };
+
+    #[sqlx::test(migrator = "MIGRATOR")]
+    async fn search_test(pool: SqlitePool) -> Result<(), Box<dyn std::error::Error>> {
+        let mut conn = pool.acquire().await?;
+        let repository = SqliteProductRepository::new(pool);
+        sqlx::query(
+            "INSERT INTO
+                m_products (
+                    name,
+                    code,
+                    unit,
+                    default_price,
+                    standard_stock_quantity
+                ) 
+                VALUES (
+                    \"商品1\",
+                    \"product001\",
+                    \"個\",
+                    2000,
+                    10
+            )",
+        )
+        .execute(&mut conn)
+        .await?;
+
+        let usecase = SearchProductUsecase::new(Box::new(repository));
+        let input = SearchProductInput::new();
+        let outputs = usecase.search(input).await?;
+        println!("{:?}", outputs);
+        Ok(())
     }
 }
