b3a7afd52995a89ec39333d48358c733f4b467c3
ofuji-works <ofuji.works@gmail.com>
2023-05-05 01:46:13 +0900

    feat(fe): add tauri core command utils

---
+++ src/utils/tauri.ts
@@ -0,0 +1,18 @@
+import type { InvokeArgs } from "@tauri-apps/api/tauri";
+
+const isNode = (): boolean =>
+  Object.prototype.toString.call(
+    typeof process !== "undefined" ? process : 0
+  ) === "[object process]";
+
+export async function invoke<T>(
+    cmd: string,
+    args?: InvokeArgs | undefined
+  ): Promise<T> {
+    if (isNode()) {
+      return Promise.resolve(undefined as unknown as T);
+    }
+    const tauriAppsApi = await import("@tauri-apps/api");
+    const tauriInvoke = tauriAppsApi.invoke;
+    return tauriInvoke(cmd, args);
+  }
