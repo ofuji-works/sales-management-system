161ff852df4910ea0e9cc804e915579c89323515
ofuji-works <ofuji.works@gmail.com>
2023-05-11 22:59:05 +0900

    feat: add domain of customer

---
+++ src-tauri/src/domain/customer.rs
@@ -0,0 +1,67 @@
+use serde::Serialize;
+use time::PrimitiveDateTime;
+
+pub type Id = i64;
+pub type Name = String;
+pub type Postal = i64;
+pub type Address = String;
+
+#[derive(Serialize, Debug)]
+pub struct Customer {
+    id: Id,
+    name: Name,
+    postal: Postal,
+    address: Address,
+    created_at: PrimitiveDateTime,
+    updated_at: PrimitiveDateTime,
+    deleted_at: Option<PrimitiveDateTime>,
+}
+impl Customer {
+    pub fn new (
+        id: Id,
+        name: Name,
+        postal: Postal,
+        address: Address,
+        created_at: PrimitiveDateTime,
+        updated_at: PrimitiveDateTime,
+        deleted_at: Option<PrimitiveDateTime>
+    ) -> Self {
+        Self {
+            id,
+            name,
+            postal,
+            address,
+            created_at,
+            updated_at,
+            deleted_at,
+        }
+    }
+
+    pub fn id(&self) -> &Id {
+        &self.id
+    }
+
+    pub fn name(&self) -> &Name {
+        &self.name
+    }
+
+    pub fn postal(&self) -> &Postal {
+        &self.postal
+    }
+
+    pub fn address(&self) -> &Address {
+        &self.address
+    }
+
+    pub fn created_at(&self) -> &PrimitiveDateTime {
+        &self.created_at
+    }
+
+    pub fn updated_at(&self) -> &PrimitiveDateTime {
+        &self.updated_at
+    }
+
+    pub fn deleted_at(&self) -> &Option<PrimitiveDateTime> {
+        &self.deleted_at
+    }
+}
--- src-tauri/src/domain/mod.rs
+++ src-tauri/src/domain/mod.rs
@@ -1,2 +1,3 @@
+pub mod customer;
 pub mod error;
 pub mod product;
